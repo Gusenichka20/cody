a63f8693cfc448a081149ec0a0a8a389782681d2
Gusenichka <SENIA-159@mail.ru>
2023-06-14 18:41:01 +0500

    initial commit

---
+++ lesson 1.py
@@ -0,0 +1,30 @@
+'''
+def strcounter(string):
+    for symbol in string:
+        counter = 0
+        for syb_symbol in string:
+            if symbol == syb_symbol:
+                counter += 1
+        print(symbol, counter)
+strcounter('aaabbbcccdddeee')
+'''
+
+'''
+def strcounter(string):
+    for symbol in set(string):
+        counter = 0
+        for syb_symbol in string:
+            if symbol == syb_symbol:
+                counter += 1
+        print(symbol, counter)
+
+
+strcounter('aaabbbcccdddeee')
+'''
+
+def strcounter(string):
+    syms_counter = {}
+    for symbol in string:
+        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+    print(syms_counter)
+strcounter('aaabbbcccdddeee')
